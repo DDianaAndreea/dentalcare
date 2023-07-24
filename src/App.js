3cef7fd3141f9a2956ac96a3b88d13a4dab3b903
DDianaAndreea <dyusk_96@yahoo.com>
2023-07-24 23:27:31 +0300

    add appointment section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
 import RowCardsSection from './components/row-cards-section/RowCardsSection';
+import Appointement from './components/appointment-section/Appointement';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Home />
       <RowCardsSection />
+      <Appointement />
     </div>
   );
 }
---
+++ src/components/appointment-section/Appointement.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Appointement.css'
+import image from "./img.png"; 
+
+function Appointement() {
+  return (
+    <div className='section'>
+        <div className='img_section'>
+        <img src={image} alt=''/>
+        </div>
+        <div className='info_section'>
+            <p><span className='p_1'><b>Speed:</b></span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p><span className='p_2'><b>Simplicity:</b></span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p><span className='p_3'><b>Quality:</b></span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <button>Request Appointement</button>
+        </div>
+    </div>
+  )
+}
+
+export default Appointement
